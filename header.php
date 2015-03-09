f7a67e0ab83902b77593059797639bd564b77e77
Martin Young <martin@digitalmyway.co.uk>
2015-03-09 13:43:48 +0000

    Add viewport meta

--- header.php
+++ header.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?>>
     <head>
         <meta charset="<?php bloginfo( 'charset' ); ?>" />
+        <meta name="viewport" content="width=device-width">
         <title><?php wp_title(); ?></title>
         <link rel="profile" href="http://gmpg.org/xfn/11" />
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>" type="text/css" media="screen" />
